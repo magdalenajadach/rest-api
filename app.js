1ccbe196d924a394386981a7a0f4f95765cb13af
Magdalena Jadach <magdalena.jadach@gmail.com>
2017-05-29 14:21:21 +0100

    Add return codes and rewrite Delete funcionality

--- app.js
+++ app.js
@@ -8,41 +8,39 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
 
-
 var check;
 
-db.serialize(function() {
+//db.serialize(function() {
   db.run("CREATE TABLE if not exists users (username TEXT, email TEXT, password TEXT)");
-})
+//})
 
 app.get('/api/v1/users/', function(req, res) {
   console.log('landed in users\n')
-  db.all("SELECT * FROM users", function(err, users) {
-    res.json({users});
+  db.all("SELECT username FROM users", function(err, users) {
+    res.status(200).json({users});
   });
 })
 
 app.get('/api/v1/users/:userName', function(req, res) {
-  console.log("SELECT * from users where userName = '" + req.params.userName + "'")
+  console.log("SELECT username from users where userName = '" + req.params.userName + "'")
   db.all("SELECT * from users where userName = '" + req.params.userName + "'", function(err, users){
-    res.json({users})
+    res.status(200).json({users})
   });
-  res.send('Single user selected\n')
 })
 
 app.post('/api/v1/users/', function(req, res) {
   var stmt = db.prepare("INSERT into users VALUES (?, ?, ?)", req.body.username, req.body.email, req.body.password);
   stmt.run();
   stmt.finalize();
-  res.send('User added\n')
+  res.status(200).send('Success - user added');
 })
 
-app.delete('/api/v1/users/', function(req, res) {
+app.delete('/api/v1/users/:userName', function(req, res) {
   console.log(req.body);
-  var stmt = db.prepare("DELETE from users where username = ?", req.body.username);
+  var stmt = db.prepare("DELETE from users where username = '" + req.params.userName + "'")
   stmt.run();
   stmt.finalize();
-  res.send('landed in users delete\n')
+  res.status(200).send('landed in users delete\n')
 })
 
 app.listen(8000, function() { 
